466e9a2f4ee0bf9ade65b3c2ac9614a6c0015e7e
yeungrak <tifshadk@gmail.com>
2025-05-18 16:14:57 +0900

    Initial Commit

---
+++ PoketmonBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PoketmonBook
+//
+//  Created by 최영락 on 5/18/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
